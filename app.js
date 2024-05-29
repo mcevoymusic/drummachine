51e6d3dbb101266a762b01f16cb8dd7cb25807a0
mcevoymusic <157783700+mcevoymusic@users.noreply.github.com>
2024-05-29 17:08:04 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const rows = document.querySelector(".sequencer").children;
 
-const kick = new Audio("https://github.com/mcevoymusic/drummachine/main/Sounds/Kick.wav"),
-	clap = new Audio("https://github.com/mcevoymusic/drummachine/main/Sounds/Snare.wav"),
-	hihat = new Audio("https://github.com/mcevoymusic/drummachine/main/Sounds/Hat.wav"),
-	rim = new Audio("https://github.com/mcevoymusic/drummachine/main/Sounds/Rim.wav"),
+const kick = new Audio("https://github.com/mcevoymusic/drummachine/raw/main/Sounds/Kick.wav"),
+	clap = new Audio("https://github.com/mcevoymusic/drummachine/raw/main/Sounds/Snare.wav"),
+	hihat = new Audio("https://github.com/mcevoymusic/drummachine/raw/main/Sounds/Hat.wav"),
+	rim = new Audio("https://github.com/mcevoymusic/drummachine/raw/main/Sounds/Rim.wav"),
 	Q = new Audio("https://raw.githubusercontent.com/kucerajacob/DRUM-SEQUENCER/master/audio/Q.mp3"),
 	W = new Audio("https://raw.githubusercontent.com/kucerajacob/DRUM-SEQUENCER/master/audio/W.mp3"),
 	E = new Audio("https://raw.githubusercontent.com/kucerajacob/DRUM-SEQUENCER/master/audio/E.mp3"),
